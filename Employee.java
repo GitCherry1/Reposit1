57fc96b5eea9fcf114dd9fefdc59b3e1401ff300
GitCherry1 <31882699+GitCherry1@users.noreply.github.com>
2017-09-12 16:15:14 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,77 @@
+package com.example.domain.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "ITEmployee")
+public class Employee {
+
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String location;
+	private String designation;
+	private int age;
+	private int salary;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", location=" + location
+				+ ", designation=" + designation + ", age=" + age + ", salary=" + salary + "]";
+	}
+	public Employee(String id, String firstName, String lastName, String location, String designation, int age,
+			int salary) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.location = location;
+		this.designation = designation;
+		this.age = age;
+		this.salary = salary;
+	}
+	public Employee() {
+		
+	}
+
+  }
